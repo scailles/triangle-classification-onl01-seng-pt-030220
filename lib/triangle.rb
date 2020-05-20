9ebf8a958b46abbd027788c39a152630645a349a
Shaina C <scailles23@gmail.com>
2020-05-20 08:32:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+  
+  def kind
+    if (@s1 == @s2) && (@s2 == @s3)
+      :equilateral
+      elsif (@s1 == @s2) || (@s2 == @s3) || (@s1 == @s3)
+        :isosceles
+      elsif (@s1 != @s2) && (@s2 != @s3) && (@s1 != @s3)
+        :scalene
+      elsif (@s1 < 0) || (@s2 < 0) || (@s3 < 0)
+        :illegal
+      end
+    end
+  
+  
+  
+  
+  class TriangleError < StandardError
+end
+
+
+
 end
